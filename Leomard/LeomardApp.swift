20c8efe9036e8d6c054315021910f6456a166a57
Athlon007 <aathlon@outlook.com>
2023-07-01 14:28:11 +0200

    Initial Commit

---
+++ Leomard/LeomardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeomardApp.swift
+//  Leomard
+//
+//  Created by Konrad Figura on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeomardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
