51da6e355ef379e1ae0947a3564cfb852dade3ac
Athlon007 <aathlon@outlook.com>
2023-07-20 11:45:39 +0200

    TagNameVerisonTest

--- Leomard/Models/GitHub/Release.swift
+++ Leomard/Models/GitHub/Release.swift
@@ -14,4 +14,16 @@ struct Release: Codable {
     let publishedAt: Date
     let assets: [ReleaseAsset]
     let body: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.htmlUrl = try container.decode(String.self, forKey: .htmlUrl)
+        self.tagName = try container.decode(String.self, forKey: .tagName)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.assets = try container.decode([ReleaseAsset].self, forKey: .assets)
+        self.body = try container.decode(String.self, forKey: .body)
+        
+        let publishedAtString = try container.decode(String.self, forKey: .publishedAt)
+        self.publishedAt = try DateFormatConverter.formatToDate(from: publishedAtString)
+    }
 }
---
+++ LeomardTests/TagNameVersionTests.swift
@@ -0,0 +1,54 @@
+//
+//  TagNameVersionTests.swift
+//  LeomardTests
+//
+//  Created by Konrad Figura on 20/07/2023.
+//
+
+import XCTest
+@testable import Leomard
+
+final class TagNameVersionTests: XCTestCase {
+    
+    func testVersion110IsNewerThan100() throws {
+        let versionA = TagNameVersion(major: 1, minor: 1, build: 0)
+        let versionB = TagNameVersion(major: 1, minor: 0, build: 0)
+        
+        XCTAssertGreaterThan(versionA, versionB)
+    }
+    
+    func testVersion111IsNewerThan110() throws {
+        let versionA = TagNameVersion(major: 1, minor: 1, build: 1)
+        let versionB = TagNameVersion(major: 1, minor: 1, build: 0)
+        
+        XCTAssertGreaterThan(versionA, versionB)
+    }
+    
+    func testVersion120IsNewerThan111() throws {
+        let versionA = TagNameVersion(major: 1, minor: 2, build: 0)
+        let versionB = TagNameVersion(major: 1, minor: 1, build: 1)
+        
+        XCTAssertGreaterThan(versionA, versionB)
+    }
+    
+    func testVersion200IsNewerThan111() throws {
+        let versionA = TagNameVersion(major: 2, minor: 0, build: 0)
+        let versionB = TagNameVersion(major: 1, minor: 1, build: 1)
+        
+        XCTAssertGreaterThan(versionA, versionB)
+    }
+    
+    func testVersion100IsEqual100() throws {
+        let versionA = TagNameVersion(major: 1, minor: 0, build: 0)
+        let versionB = TagNameVersion(major: 1, minor: 0, build: 0)
+    
+        XCTAssertEqual(versionA, versionB)
+    }
+    
+    func testVersion100IsNotNewerThan110() throws {
+        let versionA = TagNameVersion(major: 1, minor: 0, build: 0)
+        let versionB = TagNameVersion(major: 1, minor: 1, build: 0)
+        
+        XCTAssertLessThan(versionA, versionB)
+    }
+}
