fca582ef820aea0ba11ce4cb5f417d7c217727f1
Athlon007 <aathlon@outlook.com>
2023-07-28 23:33:23 +0200

    Update NavbarView.swift

--- Leomard/Views/NavbarView.swift
+++ Leomard/Views/NavbarView.swift
@@ -23,6 +23,9 @@ struct NavbarView: View {
     
     @State var addedLetters: [Character] = []
     
+    @State var searchQuery: String = ""
+    @State var searchVisible: Bool = false
+    
     var body: some View {
         sidebarItems(options: options)
             .padding(.top, 24)
@@ -48,21 +51,49 @@ struct NavbarView: View {
     
     @ViewBuilder
     private var followedCommunitiesList: some View {
-        List {
-            HStack(spacing: 14) {
-                Text("Followed")
-                Image(systemName: "v.circle")
-                    .foregroundColor(.secondary)
-                    .rotationEffect(.degrees(followedVisible ? 180 : 0))
+        VStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    HStack(spacing: 14) {
+                        Text("Followed")
+                        Image(systemName: "v.circle")
+                            .foregroundColor(.secondary)
+                            .rotationEffect(.degrees(followedVisible ? 180 : 0))
+                    }
+                    .onTapGesture {
+                        followedVisible = !followedVisible
+                    }
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(searchVisible ? Color(.linkColor) : .secondary)
+                        .onTapGesture {
+                            searchVisible = !searchVisible
+                            if !searchVisible {
+                                searchQuery = ""
+                            }
+                        }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                if searchVisible {
+                    TextField("Search", text: $searchQuery)
+                        .textFieldStyle(.roundedBorder)
+                }
             }
-            .onTapGesture {
-                followedVisible = !followedVisible
-            }
-            if followedVisible {
-                ForEach(followedCommunities.sorted(by: {
-                    UserPreferences.getInstance.preferDisplayNameCommunityFollowed ? $0.community.title < $1.community.title
-                                                                                    : $0.community.name < $1.community.name
-                }), id: \.self) { communityView in
+            .padding(.leading)
+            .padding(.trailing)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            List {
+                if followedVisible {
+                    ForEach(followedCommunities.sorted(by: {
+                        UserPreferences.getInstance.preferDisplayNameCommunityFollowed ? $0.community.title < $1.community.title
+                        : $0.community.name < $1.community.name
+                    }).filter {
+                        if searchQuery.count > 0 {
+                            return $0.community.name.contains(searchQuery) || $0.community.title.contains(searchQuery)
+                        } else {
+                            return true
+                        }
+                    }
+                            , id: \.self) { communityView in
                         if UserPreferences.getInstance.navbarShowLetterSeparators,
                            let firstChar =  UserPreferences.getInstance.preferDisplayNameCommunityFollowed ? communityView.community.title.first : communityView.community.name.first,
                            isFirstCommunityStartingWithThisChar(community: communityView.community) {
@@ -70,8 +101,11 @@ struct NavbarView: View {
                         }
                         NavbarCommunityItem(community: communityView.community, currentCommunity: $currentCommunity, contentView: contentView)
                     }
+                }
             }
+            .frame(maxHeight: .infinity)
         }
+        .frame(maxHeight: .infinity)
     }
     
     @ViewBuilder
@@ -83,6 +117,6 @@ struct NavbarView: View {
     }
     
     func isFirstCommunityStartingWithThisChar(community: Community) -> Bool {
-        return followedCommunities.filter { $0.community.name.first == community.name.first }.first?.community == community
+        return followedCommunities.filter { $0.community.name.first!.uppercased() == community.name.first!.uppercased() }.first?.community == community
     }    
 }
